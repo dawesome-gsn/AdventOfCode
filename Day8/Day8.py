016c7963483542f4c619de8e14fcb85be2cd1704
Michael Dawe <mdawe@gsn.com>
2016-01-18 13:35:48 -0500

    Day8 part 2 complete

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -1,39 +1,54 @@
 import re
 
-codeChars = 0
+totalOriginalCodeChars = 0
+totalEncodedCodeChars = 0
 stringChars = 0
 
 hexidecimalMatch = re.compile("\\\\x[0-9A-Fa-f][0-9A-Fa-f]")
 
+
+def decode(line):
+    convertedLine = line.replace("\\\\", "B")  # replace \\ with 1 character
+    convertedLine = convertedLine.replace("\\\"", '"')
+    # print strippedline
+    convertedLine = re.sub(hexidecimalMatch, "A", convertedLine)
+    lineStringChars = len(convertedLine)
+
+    return lineStringChars
+
+def encode(line):
+    encodedLine = line
+
+    #print "encoded line = " + encodedLine
+    encodedLine = encodedLine.replace("\\", "\\\\")
+    #print "after first replace = " + encodedLine
+    encodedLine = encodedLine.replace("\"", "\\\"")
+    #print "after second replace = " + encodedLine
+
+    #print line, encodedLine, len(encodedLine) + 2
+    return encodedLine, len(encodedLine) + 2 #plus 2 for the outside quotes
+
+#with open("Day8TestInput.txt") as f:
 with open("Day8Input.txt") as f:
     for line in f.readlines():
-        lineCodeChars = 0
-        lineStringChars = 0
-
         strippedLine = line.strip()
+        lineCodeChars = len(strippedLine)
 
-        lineStringChars = len(strippedLine)
+        noQuotesLine = strippedLine[1:-1]
+        print "noQuotesLine = " + noQuotesLine
+        encodedLine, encodedLineCodeChars = encode(strippedLine)
+
+        # strip the quotes from the outside
+        lineStringChars = decode(noQuotesLine)
+
+        print strippedLine, lineCodeChars, lineStringChars
+        print encodedLine, encodedLineCodeChars
+
         stringChars += lineStringChars
+        totalOriginalCodeChars += lineCodeChars
+        totalEncodedCodeChars += encodedLineCodeChars
 
-        #strip the quotes from the outside
-        strippedLine = strippedLine[1:-1]
-        convertedLine = strippedLine.replace("\\\\", "B") #replace \\ with 1 character
-        convertedLine = convertedLine.replace("\\\"",'"')
-        #print strippedLine
-        convertedLine = re.sub(hexidecimalMatch, "A", convertedLine)
-        lineCodeChars = len(convertedLine)
 
-        #splitLine = strippedLine.split("\\")
-        # for item in splitLine:
-        #     #print item
-        #     lineCodeChars += len(item)
-        #     # if len(item) > 0 and item[0] == 'x' and hexidecimalMatch.match(item[1:]):
-        #     #     lineCodeChars -= 2
-        codeChars += lineCodeChars
-
-        print line.strip()
-        print convertedLine, lineStringChars,lineCodeChars
-        print
-
-    print stringChars, codeChars
-    print stringChars - codeChars
+    print stringChars, totalOriginalCodeChars, totalEncodedCodeChars
+    print totalOriginalCodeChars - stringChars
+    print totalEncodedCodeChars - totalOriginalCodeChars
