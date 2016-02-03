93b964cb25efe0d17044584af4223214c6ead243
Michael Dawe <mdawe@gsn.com>
2016-02-03 09:15:51 -0500

    Day18 finished!

--- Day18/Day18.py
+++ Day18/Day18.py
@@ -10,15 +10,16 @@ def neighborsOn(x,y,lights):
     return neighbors.count('#')
 
 def step(lights, nextLights):
-    nextLights = [['.' for x in range(6)] for x in range(6)]
-    for x in range(6):
-        for y in range(6):
+    nextLights = [['.' for x in range(len(lights))] for x in range(len(lights))]
+    for x in range(len(lights)):
+        for y in range(len(lights)):
             num = neighborsOn(x, y, lights)
             if lights[x][y] == '.' and num == 3:
                 nextLights[x][y] = '#'
             if lights[x][y] == '#':
                 if num == 2 or num == 3:
                     nextLights[x][y] = '#'
+    nextLights[0][0] = nextLights[len(lights)-1][0] = nextLights[0][len(lights)-1] = nextLights[len(lights)-1][len(lights)-1] = '#'
     return nextLights
 
 lights = [['.' for x in range(6)] for x in range(6)]
@@ -30,14 +31,40 @@ lights[2][0] = lights[2][5] = '#'
 lights[3][2] = '#'
 lights[4][0] = lights[4][2] = lights[4][5] = '#'
 lights[5][0] = lights[5][1] = lights[5][2] = lights[5][3] = '#'
+lights[0][0] = lights[len(lights)-1][0] = lights[0][len(lights)-1] = lights[len(lights)-1][len(lights)-1] = '#'
+
 
 # print neighborsOn(0,0, lights)
 # print neighborsOn(5,5, lights)
 # print neighborsOn(0,4, lights)
 
-for x in range(4):
+for x in range(5):
     lights = step(lights, nextLights)
 
 for x in lights:
     print x
+print sum(x.count('#') for x in lights)
+
+
+lights = list()
+with open("Input.txt") as f:
+    for line in f.readlines():
+        lightLine = list()
+        for char in line:
+            lightLine.append(char)
+        lights.append(lightLine)
+lights[0][0] = lights[len(lights)-1][0] = lights[0][len(lights)-1] = lights[len(lights)-1][len(lights)-1] = '#'
+
+for x in lights:
+    print x
+print
+print
+
+for x in range(100):
+    lights = step(lights, nextLights)
+    for x in lights:
+        print x
+    print
+    print
+
 print sum(x.count('#') for x in lights)
